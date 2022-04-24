7f4df571f19fa5e43d3a2cdeccfac4b285d2b5d6
shayro9 <shayro13@gmail.com>
2022-04-24 18:43:07 +0300

    added 4,6,8 functions
    4 - get list size
    6 - get char at index
    8 - toString() of a list

--- RLEList.c
+++ RLEList.c
@@ -15,3 +15,48 @@ void RLEListDestroy(RLEList list)
     RLEListDestroy(list->next);
     free(list);
 }
+
+int RLEListSize(RLEList list)
+{
+    if(!list){
+        return -1;
+    }
+    if(!list->next){
+        return list->len;
+    }
+    return list->len + RLEListSize(list->next);
+}
+
+char RLEListGet(RLEList list, int index, RLEListResult *result)
+{
+    int size = RLEListSize(list);
+    if(!list)
+        *result = RLE_LIST_NULL_ARGUMENT;
+    if(index >= size)
+        *result = RLE_LIST_INDEX_OUT_OF_BOUNDS;
+    if(*result != RLE_LIST_SUCCESS)
+        return 0;
+
+    if(index <= list->len) {
+        *result = RLE_LIST_SUCCESS;
+        return list->val;
+    }
+    return RLEListGet(list->next,index - list->len,result);
+
+}
+
+char* RLEListExportToString(RLEList list, RLEListResult* result)
+{
+    if(!list){
+        *result = RLE_LIST_NULL_ARGUMENT;
+        return NULL;
+    }
+    int size = RLEListSize(list);
+    char* out = malloc(size-1);
+    for (int i = 0; i < size; ++i) {
+        char curr = RLEListGet(list,i,result);
+        out[i] = curr;
+    }
+    *result = RLE_LIST_SUCCESS;
+    return out;
+}
---
+++ cmake-build-debug/CMakeFiles/ex1_mtm.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex1_mtm.dir/RLEList.c.obj"
+  "CMakeFiles/ex1_mtm.dir/tests/RLEList_example_test.c.obj"
+  "ex1_mtm.exe"
+  "ex1_mtm.exe.manifest"
+  "ex1_mtm.lib"
+  "ex1_mtm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ex1_mtm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
